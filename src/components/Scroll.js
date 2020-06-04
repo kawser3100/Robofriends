30bd2babe63846bd9751bdec9b31ea1ee2dbde3d
Kawser Ahmed <write2kawsera@gmail.com>
2020-06-04 16:35:00 +0600

    Added actions, reducers and constants file for Redux

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-        <div style={{ overflow: 'scroll' , border: '1px solid black', height: '800px'}}>
+        <div style={{ overflow: 'scroll' , border: '1px solid black', height: '1000px'}}>
             {props.children}
         </div>
     );
