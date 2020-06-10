1e3deb2adfdb1f791fa7824292939b39afea5995
Kawser Ahmed <write2kawsera@gmail.com>
2020-06-10 15:43:27 +0600

    async action now work with redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore , applyMiddleware } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore , applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './container/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'; 
 
+
 const logger= createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger))
+
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger))
 
 ReactDOM.render(
                 <Provider store={ store }>
